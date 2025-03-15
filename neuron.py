1cc179690c269fc9c8a4517fdf9234262644931d
Steven Cheung <stevencheung0690@outlook.com>
2025-03-15 00:34:44 +0000

    style(neuron): added docstrings

--- neuron.py
+++ neuron.py
@@ -11,7 +11,17 @@ class Neuron:
     output:Optional[float]
 
     def set_output(self, output:Optional[float]) -> None:
+        """_summary_
+
+        Args:
+            output (Optional[float]): The result of the neuron's activation function
+        """
         self.output = output
 
     def set_delta(self, error:float) -> None:
+        """_summary_
+
+        Args:
+            error (float): Used during backpropagation process to adjust weights
+        """
         self.data = error * sigmoid_derivative(self.output)
