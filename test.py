f41935a841d47f68bca3f2ce4f9b20eaca1ced5d
Steven Cheung <stevencheung0690@outlook.com>
2025-03-16 18:37:47 +0000

    test: added testing of activation sum

--- test.py
+++ test.py
@@ -4,4 +4,14 @@ from neuron import *
 
 class TestNeuron(unittest.TestCase):
     def test_activation_sum(self):
-        pass
+        weights = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
+        bias = 0.5
+        delta = 0
+        output = 0
+        neuron = Neuron(weights, bias, delta, output)
+        data_input = [2.78, 2.55]
+        result = neuron.weighted_sum(data_input)
+        self.assertEqual(result, 6)
+
+if __name__ == "__main__":
+    unittest.main()
