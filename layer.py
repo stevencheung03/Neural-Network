9c5e6b1c9ce0a46b3661da273e4dbb0b95609894
Steven Cheung <stevencheung0690@outlook.com>
2025-03-26 21:47:31 +0000

    feat(layer): added functions

--- layer.py
+++ layer.py
@@ -6,3 +6,12 @@ from neuron import *
 @dataclass
 class Layer:
     neurons: List[Neuron]
+
+    def all_outputs(self, inputs: List[float]) -> List[float]:
+        return [neuron.output for neuron in self.neurons]
+    
+    def activate_neurons(self, input: List[float]) -> List[float]:
+        return [neuron.activate(input) for neuron in self.neurons]
+    
+    def total_delta(self, previous_layer_neuron: int) -> List[float]:
+        return sum(neuron.weights[previous_layer_neuron] * neuron.delta for neuron in self.neurons)
