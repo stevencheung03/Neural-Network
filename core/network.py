1218c524a481cdba676de147c96a62b9f6b94082
Steven Cheung <stevencheung0690@outlook.com>
2025-05-31 19:37:13 +0100

    feat: forward propagation

--- core/network.py
+++ core/network.py
@@ -11,3 +11,8 @@ class Network:
 
     def layers(self) -> List[Layer]:
         return self.hidden_layers + [self.output_layers]
+    
+    def feed_forward(self, inputs: List[float]) -> List[float]:
+        for layer in self.hidden_layers:
+            inputs = layer.activate_neurons(inputs)
+        return self.output_layers.activate_neurons(inputs)
