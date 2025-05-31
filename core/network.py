4f9c17815839565df8010b09a3f2d28a1cd31a47
Steven Cheung <stevencheung0690@outlook.com>
2025-05-31 18:32:49 +0100

    feat: return all output and hidden layers

--- core/network.py
+++ core/network.py
@@ -8,3 +8,6 @@ class Network:
     hidden_layers: List[Layer]
     output_layers: Layer
     learning_rate: float
+
+    def layers(self) -> List[Layer]:
+        return self.hidden_layers + [self.output_layers]
